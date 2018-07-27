a8e5cc278e72d78afa6f39a838493f682bde06e8
Brett J Norris <brettjnorris@gmail.com>
2018-07-27 00:22:43 -0400

    Add logout route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
-    self.current_user = @user
+    session[:user_id] = @user.id
     redirect_to '/'
+  end
+
+  def destroy
+    if current_user
+      session.delete(:user_id)
+      flash[:success] = "Sucessfully logged out!"
+    end
+    redirect_to root_path
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
+
   get '/auth/:provider/callback', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 end
